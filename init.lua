988d8862b897f4404dfc158adcb692c95f38efd2
Ethan Morgan <ethan@gweithio.com>
2024-10-07 13:31:56 +0100

    updates

--- init.lua
+++ init.lua
@@ -178,23 +178,4 @@ end
 --  text_format = text_format,
 --})
 
-require("presence").setup({
-  auto_update = true,
-  main_image = "neovim",
-  log_level = nil,
-  debounce_timeout = 10,
-  enable_line_number = true,
-  blacklist = {},
-  buttons = true,
-  file_assets = {},
-  show_time = true,
-  editing_text = "Editing %s",
-  file_explorer_text = "Browsing %s",
-  git_commit_text = "Committing changes",
-  plugin_manager_text = "Managing plugins",
-  reading_text = "Reading %s",
-  workspace_text = "Working on %s",
-  line_number_text = "Line %s out of %s",
-})
-
 require 'ocaml_mlx'
--- lua/sixfourtwelve/lazy.lua
+++ lua/sixfourtwelve/lazy.lua
@@ -1,9 +1,33 @@
 vim.g.mapleader = " "
 
 return require("lazy").setup({
+  {
+    "scalameta/nvim-metals",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    ft = { "scala", "sbt", "java" },
+    opts = function()
+      local metals_config = require("metals").bare_config()
+      metals_config.on_attach = function(client, bufnr)
+        -- your on_attach function
+      end
+
+      return metals_config
+    end,
+    config = function(self, metals_config)
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = self.ft,
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end
+  },
   { "github/copilot.vim" },
   { "ocaml-mlx/ocaml_mlx.nvim" },
-  { 'andweeb/presence.nvim' },
   {
     'Wansmer/symbol-usage.nvim',
     event = 'LspAttach',
@@ -11,8 +35,6 @@ return require("lazy").setup({
   { "ionide/Ionide-vim" },
   { 'reasonml-editor/vim-reason-plus' },
   { 'danielo515/nvim-treesitter-reason' },
-  { 'rescript-lang/vim-rescript',       ft = "rescript" },
-  { 'andweeb/presence.nvim' },
   { 'rescript-lang/vim-rescript',       ft = "rescript" },
   {
     "karb94/neoscroll.nvim",
